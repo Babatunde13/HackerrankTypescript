71e0a213e41e36b70382972445a686b96715fbec
Babatunde Koiki <koikibabatunde14@gmail.com>
2021-04-26 22:59:04 +0100

    day 1 task

---
+++ src/birthday.Cake.Candle.ts
@@ -0,0 +1,7 @@
+const birthdayCakeCandles = (candles: number[]) : number => {
+  let count : number = 0
+  let max = Math.max(...candles)
+  candles.forEach((ele: number) => ele === max? count ++ : count)
+  return count
+}
+console.log(birthdayCakeCandles([4, 4, 1, 3]))
